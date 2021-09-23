7f054f1ca0ba69cfbf840d0b7bd30ab76ab241ef
Jack Hsu <jack.hsu@heyu.tw>
2021-09-23 17:40:40 +0800

    fix:調整migrate

--- database/migrations/2021_09_23_085547_init.php
+++ database/migrations/2021_09_23_085547_init.php
@@ -25,12 +25,14 @@ class Init extends Migration
             $table->bigIncrements('id');
             $table->string('object_id', 64)->comment('唯一識別碼');
             $table->string('session_id', 64)->comment('用戶識別碼');
-            $table->tinyint('action')->comment('動作');
-            $table->tinyint('type')->comment('類型');
+            $table->tinyInteger('action')->comment('動作');
+            $table->tinyInteger('type')->comment('類型');
             $table->float('value', 12, 4)->default(0.0)->comment('價值');
             $table->string('currency', 24)->comment('幣別');
             $table->timestamps();
         });
+
+        Schema::dropIfExists('personal_access_tokens');
     }
 
     /**
@@ -41,6 +43,6 @@ class Init extends Migration
     public function down()
     {
         Schema::dropIfExists('tbl_tags');
-        Schema::dropIfExists('tbl_tags_record');
+        Schema::dropIfExists('tbl_tag_record');
     }
 }
